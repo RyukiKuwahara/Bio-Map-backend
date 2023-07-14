436d73455a0d44afe7214fd0b3ebf1be59a5ab18
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-07-14 15:58:40 +0900

    #2 CORSを設定

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,6 @@ module github.com/RyukiKuwahara/Bio-Map/backend
 go 1.18
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,23 @@ import (
 func main() {
 	http.HandleFunc("/users", handlers.CreateUserHandler)
 
-	err := http.ListenAndServe(":8080", nil)
+	cors := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == "OPTIONS" {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+				return
+			}
+
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	err := http.ListenAndServe(":8080", cors(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal("Server failed to start: ", err)
 	}
