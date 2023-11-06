f193383010ee6bcdc379c7b407fd4a2abeb0dccc
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-11-06 23:04:34 +0900

    #19 構造体でデコードするように変更

--- handlers/logoutHandlers.go
+++ handlers/logoutHandlers.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/RyukiKuwahara/Bio-Map/models"
 	"github.com/RyukiKuwahara/Bio-Map/services"
 )
 
 func LogoutUserHandler(w http.ResponseWriter, r *http.Request) {
-	var sessionId string
-	err := json.NewDecoder(r.Body).Decode(&sessionId)
+	var data models.SessionData
+	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	err = services.LogoutUser(sessionId)
+	err = services.LogoutUser(data.SessionId)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
